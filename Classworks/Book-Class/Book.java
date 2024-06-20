e92b9223e7dda414fed0650d99d171c46e0b9f58
Alex <127744440+4Al3xG4mesYY@users.noreply.github.com>
2024-06-20 17:46:54 -0400

    Update Book.java

--- Classworks/Book-Class/Book.java
+++ Classworks/Book-Class/Book.java
@@ -4,20 +4,46 @@ public class Book
 	private String title;
 	private double price;
 	
-	public Book(int i, String t, double p){
-	    this.isbn = i;
-	    this.title = t;
-	    this.price = p;
-	}
-
-
-	public void read()
-	{
-	    System.out.println("Enjoy reading your book!");
-	}
+    public void setIsbn(int i)
+    {
+        isbn = i;
+    }
     
+    public void setTitle(String t)
+    {
+        title = t;
+    }
+    
+    public void setPrice(double p)
+    {
+        price = p;
+    }
+    
+    public int getIsbn()
+    {
+        return isbn;
+    }
+    
+    public String getTitle()
+    {
+        return title;
+    }
+    
+    public double getPrice()
+    {
+        return price; 
+    }
+    
+    public void read()
+    {
+	System.out.println("Enjoy reading your book!");
+    }
+	
     public static void main(String[] args) {
         Book book1 = new Book(1234567890, "The Great Gatsby", 19.99);
+            
         book1.read();
+        System.out.println(book1.getTitle());
     }
+
 }
